a12844be6aa836c04874bb6707f4c072bcf67549
repattila <repattila@protonmail.com>
2025-05-25 17:34:39 +0200

    Working on part 2, test4 not correct.

--- 12/solver.go
+++ 12/solver.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 )
 
 type pos struct {
@@ -12,8 +13,47 @@ type pos struct {
 	col int
 }
 
+type HSide []pos
+type VSide []pos
+
+func (s HSide) Len() int {
+	return len(s)
+}
+
+// Implement the Less method required by sort.Interface
+func (s HSide) Less(i, j int) bool {
+	if s[i].row == s[j].row {
+		return s[i].col < s[j].col
+	} else {
+		return s[i].row < s[j].row
+	}
+}
+
+// Implement the Swap method required by sort.Interface
+func (s HSide) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func (s VSide) Len() int {
+	return len(s)
+}
+
+// Implement the Less method required by sort.Interface
+func (s VSide) Less(i, j int) bool {
+	if s[i].col == s[j].col {
+		return s[i].row < s[j].row
+	} else {
+		return s[i].col < s[j].col
+	}
+}
+
+// Implement the Swap method required by sort.Interface
+func (s VSide) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
 func main() {
-	file, err := os.Open("input.txt")
+	file, err := os.Open("test4.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -46,35 +86,99 @@ func main() {
 	}
 
 	var res int
+	var res1 int
 	for _, region := range regions {
 		var regionId rune = field[region[0].row][region[0].col]
 
+		// Fence on the upper side of the pos
+		var horizontalSides HSide = make([]pos, 0)
+		// Fence on the left side of the pos
+		var verticalSides VSide = make([]pos, 0)
 		var fenceNum int
+
 		for _, cell := range region {
 			r := cell.row
 			c := cell.col
 
 			if r == 0 || field[r-1][c] != regionId {
 				fenceNum += 1
+
+				horizontalSides = append(horizontalSides, pos{r, c})
 			}
 			if r == len(field)-1 || field[r+1][c] != regionId {
 				fenceNum += 1
+
+				horizontalSides = append(horizontalSides, pos{r + 1, c})
 			}
 			if c == 0 || field[r][c-1] != regionId {
 				fenceNum += 1
+
+				verticalSides = append(verticalSides, pos{r, c})
 			}
 			if c == len(field[r])-1 || field[r][c+1] != regionId {
 				fenceNum += 1
+
+				verticalSides = append(verticalSides, pos{r, c + 1})
 			}
 		}
 
 		res += fenceNum * len(region)
+
+		sort.Sort(horizontalSides)
+		sort.Sort(verticalSides)
+
+		fmt.Printf("HS: %v\n", horizontalSides)
+
+		var hSideNum int = 0
+		var currRow int = -1
+		var currCol int = -1
+		for _, p := range horizontalSides {
+			if currRow != -1 {
+				if p.row != currRow {
+					hSideNum += 1
+				} else if p.col != (currCol + 1) {
+					hSideNum += 1
+				}
+			} else {
+				hSideNum += 1
+			}
+
+			currRow = p.row
+			currCol = p.col
+		}
+
+		fmt.Printf("HSNum: %v\n", hSideNum)
+
+		fmt.Printf("VS: %v\n", verticalSides)
+
+		var vSideNum int = 0
+		currRow = -1
+		currCol = -1
+		for _, p := range verticalSides {
+			if currCol != -1 {
+				if p.col != currCol {
+					vSideNum += 1
+				} else if p.row != (currRow + 1) {
+					vSideNum += 1
+				}
+			} else {
+				vSideNum += 1
+			}
+
+			currRow = p.row
+			currCol = p.col
+		}
+
+		fmt.Printf("VSNum: %v\n", vSideNum)
+
+		res1 += (hSideNum + vSideNum) * len(region)
 	}
 
-	fmt.Printf("%v\n", field)
-	fmt.Printf("%v\n", used)
-	fmt.Printf("%v\n", regions)
+	//fmt.Printf("%v\n", field)
+	//fmt.Printf("%v\n", used)
+	//fmt.Printf("%v\n", regions)
 	fmt.Printf("%v\n", res)
+	fmt.Printf("%v\n", res1)
 }
 
 func traceRegion(r int, c int, regionId rune, field [][]rune, used [][]bool, elems []pos) []pos {
