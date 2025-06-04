e6f15d3a086402442f1f712bb6a216c6ebdea2a7
repattila <repattila@protonmail.com>
2025-06-04 22:17:54 +0200

    Part 2 working

--- 13/solver.go
+++ 13/solver.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"log"
+	"math"
 	"os"
 )
 
@@ -47,8 +48,8 @@ func main() {
 		line = scanner.Text()
 		line = line[7:]
 		fmt.Sscanf(line, "X=%d, Y=%d", &x, &y)
-		m.px = x
-		m.py = y
+		m.px = x + 10000000000000
+		m.py = y + 10000000000000
 
 		machines = append(machines, m)
 
@@ -63,19 +64,74 @@ func main() {
 	fmt.Printf("%v\n", machines)
 
 	var res int
-	for _, m := range machines {
-		var minCost int = 500
-		for i := 0; i <= 100; i++ {
-			for j := 0; j <= 100; j++ {
-				if i*m.ax+j*m.bx == m.px && i*m.ay+j*m.by == m.py {
-					minCost = min(minCost, i*3+j*1)
+machineLoop:
+	for n, m := range machines {
+		fmt.Printf("Machine: %v\n", n)
+
+		var aRate float64 = float64(m.ax) / float64(m.ay)
+		var bRate float64 = float64(m.bx) / float64(m.by)
+		var pRate float64 = float64(m.px) / float64(m.py)
+
+		if almostEqual(aRate, bRate) && !almostEqual(aRate, pRate) {
+			fmt.Printf("Unsolvable\n")
+			continue machineLoop
+		} else {
+			// Solving the following equation system:
+			// i * m.ax + j * m.bx = m.px
+			// i * m.ay + j * m.by = m.py
+
+			var d int = m.ax*m.by - m.bx*m.ay
+			fmt.Printf("d: %v\n", d)
+			if d != 0 {
+				var di int = m.px*m.by - m.bx*m.py
+				var dj int = m.ax*m.py - m.px*m.ay
+
+				// We only care about solutions that give whole numbers for button presses
+				if di%d == 0 && dj%d == 0 {
+					var i int = di / d
+					var j int = dj / d
+
+					// We only care about solutions where the button presses are positive
+					if i >= 0 && j >= 0 {
+						fmt.Printf("Press A: %v, press B: %v\n\n", i, j)
+
+						res += i*3 + j
+					} else {
+						fmt.Printf("Solution has negative value\n\n")
+					}
+				} else {
+					fmt.Printf("Solution has fractions\n\n")
+				}
+			} else {
+				var multB int = m.px / m.bx
+
+				if m.px%m.bx == 0 {
+					if m.by*multB == m.py {
+						res += multB
+						continue machineLoop
+					}
+				}
+
+				for i := multB; i >= 0; i-- {
+					if (m.px-m.bx*i)%m.ax == 0 {
+						var multA int = (m.px - m.bx*i) / m.ax
+						if m.by*i+m.ay*multA == m.py {
+							fmt.Printf("Press A: %v, press B: %v\n\n", multA, i)
+
+							res += i + 3*multA
+							continue machineLoop
+						}
+					}
 				}
 			}
-		}
-		if minCost != 500 {
-			res += minCost
 		}
 	}
 
 	fmt.Printf("%v\n", res)
+}
+
+const float64EqualityThreshold = 1e-9
+
+func almostEqual(a, b float64) bool {
+	return math.Abs(a-b) <= float64EqualityThreshold
 }
